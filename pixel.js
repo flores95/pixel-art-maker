305ff74c546e962e395571b96828d0f69ee706b1
galvanize9 <galvanize9@fvfx1cf5j1wl.ci.org>
2019-05-07 14:38:53 -0600

    2x2

---
+++ pixel.js
@@ -0,0 +1,20 @@
+// const divRow = document.querySelector('.row')
+// divRow.addEventListener('click', (event) => console.log(event))
+
+const divMainContainer = document.querySelector('.main-container')
+
+const addRow = (boxCount) => {
+    const rowDiv = document.createElement('div')
+    rowDiv.className = 'row'
+
+    for(let i = 0; i < boxCount; i++) {
+        let boxDiv = document.createElement('div')
+        boxDiv.className = 'box'
+        rowDiv.appendChild(boxDiv)
+    }
+
+    return rowDiv
+}
+
+divMainContainer.appendChild(addRow(2))
+divMainContainer.appendChild(addRow(2))
