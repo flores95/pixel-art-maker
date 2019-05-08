6032d13cd878fa98aa6d59d380ec16800d17262a
Mike Flores <mflores@us.ci.org>
2019-05-07 23:31:55 -0700

    refactoring color to include white gradients

--- color.js
+++ color.js
@@ -1,6 +1,6 @@
 const ArrayRange = (range) => [...Array(range).keys()]
 
-class color {
+class Color {
   constructor(red = 255, green = 255, blue = 255, alpha = 1) {
     this.red = red
     this.green = green
@@ -14,30 +14,42 @@ class color {
     return `rgba(${this.red}, ${this.green}, ${this.blue}, ${this.alpha})`
   }
 
-  colorGradients(variableAttribute, variationCount) {
+  colorGradients(variableAttributes, variationCount) {
     const stepSize = Math.floor(255 / variationCount)
-    return ArrayRange(Math.floor(variationCount / 2))
+
+    const isWhite = (this.red === 255 && this.green === 255 && this.blue === 255)
+    const iterations = (isWhite) ? variationCount : Math.floor(variationCount) / 2
+    return ArrayRange(iterations)
       .reduce((acc, cnt) => {
         const step = (cnt + 1) * stepSize
 
-        const darker = new color(this.red, this.green, this.blue, this.alpha)
-        darker[variableAttribute] -= step
+        const darker = new Color(this.red, this.green, this.blue, this.alpha)
+        variableAttributes.forEach(attr => darker[attr] -= step)
         acc.push(darker)
 
-        const lighter = new color(this.red, this.green, this.blue, this.alpha)
-        this.colorAttributes
-          .filter(attr => attr !== variableAttribute)
-          .forEach(filtered => this[filtered] += step)
-        acc.push(lighter)
+        if (!isWhite) {
+          const lighter = new Color(this.red, this.green, this.blue, this.alpha)
+          this.colorAttributes
+            .filter(attr => !variableAttributes.includes(attr))
+            .forEach(filtered => lighter[filtered] += step)
+          acc.push(lighter)
+        }
 
         return acc
       }, [])
-      .sort((a, b) => a[variableAttribute] - b[variableAttribute])
+      .sort((a, b) => a[variableAttributes[0]] - b[variableAttributes[0]])
   }
 }
 
-const gradients = (new color(255, 0, 0, 1)).colorGradients('red', 10)
-const colors = gradients.map(color => color.css())
-console.log(colors)
+// const red = {rgb: [255, 0, 0, 1], attrs: ['red']}
+// const green = {rgb: [0, 255, 0, 1], attrs: ['green']}
+// const blue = {rgb: [0, 0, 255, 1], attrs: ['blue']}
+// const yellow = {rgb: [255, 255, 0, 1], attrs: ['red', 'green']}
+// const pink = {rgb: [255, 0, 255, 1], attrs: ['red', 'blue']}
+// const cyan = {rgb: [0, 255, 255, 1], attrs: ['green', 'blue']}
+// const white = {rgb: [255, 255, 255, 1], attrs: ['red', 'green', 'blue']}
 
-module.exports = color
+// const color = white
+// const grads = (new Color(...color.rgb)).colorGradients(color.attrs, 10)
+// colors = grads.map(grad => grad.css())
+// console.log(colors)
